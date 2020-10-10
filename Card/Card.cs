264768acb6b21e90269c164140deddc492398ba1
blaec <odeskonst@yandex.ru>
2020-10-10 16:38:01 +0300

    Add project files.

---
+++ Card/Card.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Card
+{
+    class Card
+    {
+        public Suits Suit { get; }
+        public Values Value { get; }
+
+        public Card(Suits suit, Values value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value.ToString()} of {Suit.ToString()}";
+        }
+
+        public static bool DoesCardMatch(Card cardToCheck, Suits suit)
+        {
+            return cardToCheck.Suit == suit;
+        }
+
+        public static bool DoesCardMatch(Card cardToCheck, Values value)
+        {
+            return cardToCheck.Value == value;
+        }
+    }
+}
---
+++ Card/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Card
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<Card> cards = new List<Card>();
+
+            for (int i = 0; i < 15; i++)
+            {
+                Card card = new Card((Suits)random.Next(4), (Values)random.Next(1, 14));
+                cards.Add(card);
+            }
+
+            foreach (Card card in cards)
+            {
+                Console.WriteLine($"{Card.DoesCardMatch(card, Suits.Clubs)} | {card}");
+            }
+            Console.WriteLine("------------- sorting... ---------------");
+            CardComparer_byValue comparer = new CardComparer_byValue();
+            cards.Sort(comparer);
+            foreach (Card card in cards)
+            {
+                Console.WriteLine(card);
+            }
+
+        }
+    }
+}
